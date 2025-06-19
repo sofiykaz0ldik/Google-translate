001c37497b269dc70a963c13f213d94cbafdc7ad
Sofia Trocenko <trocenko3103@mail.ru>
2025-06-19 09:24:07 +0300

    oops

--- translate.js
+++ translate.js
@@ -1,152 +1,92 @@
-const $ = document;
+class Translate {
+  constructor() {
+    this.inputText = document.querySelector('.text-input');
+    this.outputText = document.querySelector('.text-output');
 
-const divform = $.createElement("div");
-const form = $.createElement("form");
+    this.sourceButtons = document.querySelectorAll('.first-lang .lang-btn');
+    this.resultButtons = document.querySelectorAll('.second-lang .lang-btn');
 
-const textBlock = $.createElement("div");
-const textTransH = $.createElement("h3");
-textTransH.textContent = "Translate";
-textBlock.appendChild(textTransH);
-textBlock.classList.add("text-block");
+    this.defineButton = document.querySelector('.define-lang');
+    this.swapButton = document.querySelector('.swap-btn');
 
-const firstLangBlock = $.createElement("div");
-const firstLangRuBtn = $.createElement("button");
-const secondLangEngBtn = $.createElement("button");
-const defineLang = $.createElement("button");
-firstLangRuBtn.textContent = "Русский";
-secondLangEngBtn.textContent = "Английский";
-defineLang.textContent = "Определить язык";
-firstLangBlock.appendChild(defineLang);
-firstLangBlock.appendChild(firstLangRuBtn);
-firstLangBlock.appendChild(secondLangEngBtn);
-firstLangBlock.classList.add("lang-block", "first-lang");
+    this.sourceLang = 'ru';  
+    this.resultLang = 'en';  
 
-const swapBlock = $.createElement("div");
-const swapBtn = $.createElement("button");
-swapBlock.appendChild(swapBtn);
-swapBlock.classList.add("swap-block");
+    this.timeout = null;
+    this.setActiveButton(this.sourceButtons, this.sourceLang);
+    this.setActiveButton(this.resultButtons, this.resultLang);
+    this.addListeners();
+  }
+  setActiveButton(buttons, lang) {
+    buttons.forEach(btn => {
+      btn.classList.toggle('active', btn.dataset.lang === lang);
+    });
+  }
 
-const secondLangBlock = $.createElement("div");
-const threeLangRuBtn = $.createElement("button");
-const fourLangEngBtn = $.createElement("button");
-threeLangRuBtn.textContent = "Русский";
-fourLangEngBtn.textContent = "Английский";
-secondLangBlock.appendChild(threeLangRuBtn);
-secondLangBlock.appendChild(fourLangEngBtn);
-secondLangBlock.classList.add("lang-block", "second-lang");
+  addListeners() {
+    this.sourceButtons.forEach(btn => {
+      btn.addEventListener('click', () => {
+        this.sourceLang = btn.dataset.lang;
+        this.setActiveButton(this.sourceButtons, this.sourceLang);
+        this.resultLang = this.sourceLang === 'ru' ? 'en' : 'ru';
+        this.setActiveButton(this.resultButtons, this.resultLang);
+        this.translateText();
+      });
+    });
 
-const langBlock = $.createElement("div");
-langBlock.classList.add("output-row");
+    this.resultButtons.forEach(btn => {
+      btn.addEventListener('click', () => {
+        this.resultLang = btn.dataset.lang;
+        this.setActiveButton(this.resultButtons, this.resultLang);
+        this.translateText();
+      });
+    });
 
-const inputBlock = $.createElement("div");
-const textTranslateInput = $.createElement("input");
-inputBlock.appendChild(textTranslateInput);
-inputBlock.classList.add("block", "block-input");
+    this.defineButton.addEventListener('click', () => {
+      const text = this.inputText.value.trim();
+      if (!text) return;
+      const isRus = /[а-яА-ЯЁё]/.test(text);
+      this.sourceLang = isRus ? 'ru' : 'en';
+      this.setActiveButton(this.sourceButtons, this.sourceLang);
+      this.resultLang = this.sourceLang === 'ru' ? 'en' : 'ru';
+      this.setActiveButton(this.resultButtons, this.resultLang);
+      this.translateText();
+    });
 
-const outputBlock = $.createElement("div");
-const textTranslateOutput = $.createElement("input");
-textTranslateOutput.readOnly = true;
-outputBlock.appendChild(textTranslateOutput);
-outputBlock.classList.add("block", "block-output");
+    this.inputText.addEventListener('input', () => {
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(() => this.translateText(), 200);
+    });
 
-const translateBlock = $.createElement("div");
-translateBlock.classList.add("input-trans");
+    this.swapButton.addEventListener('click', () => {
+      [this.sourceLang, this.resultLang] = [this.resultLang, this.sourceLang];
+      [this.inputText.value, this.outputText.value] = [this.outputText.value, this.inputText.value];
+      this.setActiveButton(this.sourceButtons, this.sourceLang);
+      this.setActiveButton(this.resultButtons, this.resultLang);
+      this.translateText();
+    });
+  }
 
-const formBlock = $.createElement("div");
-formBlock.classList.add("form");
-
-formBlock.appendChild(langBlock);
-formBlock.appendChild(translateBlock);
-
-langBlock.appendChild(firstLangBlock);
-langBlock.appendChild(swapBlock);
-langBlock.appendChild(secondLangBlock);
-
-translateBlock.appendChild(inputBlock);
-translateBlock.appendChild(outputBlock);
-
-divform.classList.add("form-wrapper");
-divform.appendChild(formBlock);
-$.body.appendChild(divform);
-
-let sourceLang = 'ru';
-let resultLang = 'en';
-
-function setActiveButton(buttons, activeButton) {
-  buttons.forEach(btn => btn.classList.remove('active'));
-  activeButton.classList.add('active');
-}
-async function translateText() {
-    const text = textTranslateInput.value.trim();
+  async translateText() {
+    const text = this.inputText.value.trim();
     if (!text) {
-        textTranslateOutput.value = '';
-        return;
+      this.outputText.value = '';
+      return;
     }
+    this.outputText.value = 'идёт перевод...';
     try {
-        textTranslateOutput.value = "идет перевод...";
-        const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${resultLang}&dt=t&q=${encodeURIComponent(text)}`;
-        const response = await fetch(url);
-        const data = await response.json();
-        const translated = data[0].map(part => part[0]).join('');
-        textTranslateOutput.value = translated || "не удалось перевести!";
-    } catch (error) {
-        textTranslateOutput.value = "Ошибка перевода";
+      const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${this.sourceLang}&tl=${this.resultLang}&dt=t&q=${encodeURIComponent(text)}`;
+      const response = await fetch(url);
+      const data = await response.json(); //await тип ждем результата пока не заверш промис 
+      const translated = data[0].map(text => text[0]).join('');
+      this.outputText.value = translated || 'не удалось перевести!';
+    } 
+    catch {
+      this.outputText.value = 'Ошибка перевода';
     }
+  }
 }
-const sourceLangButtons = [firstLangRuBtn, secondLangEngBtn];
-const resultLangButtons = [threeLangRuBtn, fourLangEngBtn];
 
-firstLangRuBtn.addEventListener('click', () => {
-    sourceLang = 'ru';
-    setActiveButton(sourceLangButtons, firstLangRuBtn);
-    translateText();
+document.addEventListener('DOMContentLoaded', () => {
+  new Translate();
 });
-secondLangEngBtn.addEventListener('click', () => {
-    sourceLang = 'en';
-    setActiveButton(sourceLangButtons, secondLangEngBtn);
-    translateText();
-});
-threeLangRuBtn.addEventListener('click', () => {
-    resultLang = 'ru';
-    setActiveButton(resultLangButtons, threeLangRuBtn);
-    translateText();
-});
-fourLangEngBtn.addEventListener('click', () => {
-    resultLang = 'en';
-    setActiveButton(resultLangButtons, fourLangEngBtn);
-    translateText();
-});
-defineLang.addEventListener('click', () => {
-    const text = textTranslateInput.value.trim();
-    if (!text) return;
-    const isRussian = /[а-яА-ЯЁё]/.test(text);
-    sourceLang = isRussian ? 'ru' : 'en';
-    if (sourceLang === 'ru') {
-        setActiveButton(sourceLangButtons, firstLangRuBtn);
-    } else {
-        setActiveButton(sourceLangButtons, secondLangEngBtn);
-    }
-    translateText();
-});
-textTranslateInput.addEventListener('input', () => {
-    clearTimeout(window.translateTimeout );
-    window.translateTimeout  = setTimeout(translateText, 200);
-});
-
-swapBtn.addEventListener('click', () => {
-    [sourceLang, resultLang] = [resultLang, sourceLang];
-    [textTranslateInput.value, textTranslateOutput.value] = [textTranslateOutput.value, textTranslateInput.value];
-    if (sourceLang === 'ru') {
-        setActiveButton(sourceLangButtons, firstLangRuBtn);
-    } else {
-        setActiveButton(sourceLangButtons, secondLangEngBtn);
-    }
-    if (resultLang === 'ru') {
-        setActiveButton(resultLangButtons, threeLangRuBtn);
-    } else {
-        setActiveButton(resultLangButtons, fourLangEngBtn);
-    }
-    translateText();
-});
-setActiveButton(sourceLangButtons, firstLangRuBtn);
-setActiveButton(resultLangButtons, fourLangEngBtn);
